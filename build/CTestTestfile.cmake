91ab3aaad35aeb98e7c0aa5359c138ae95093d48
Ubuntu <ubuntu@ip-172-31-15-71.us-west-2.compute.internal>
2024-06-13 16:50:27 +0000

    Added build and devel

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ROS1_Go_Chase_It/src
+# Build directory: /home/ubuntu/ROS1_Go_Chase_It/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
