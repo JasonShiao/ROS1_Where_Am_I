38c4bddfa0c81d3678cc8342082c9265237301b1
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2024-06-14 12:28:00 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/ROS1_Go_Chase_It/src
-# Build directory: /home/ubuntu/ROS1_Go_Chase_It/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot")
